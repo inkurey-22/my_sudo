ac229d4799f0446166db6c56b6e54c8db6e345b7
Théophile "Inkurey" R. <inkurey22.tr@proton.me>
2025-01-28 12:55:15 +0100

    REFACTOR: verified last unverified malloc in split_string and refactored the function to fit coding style

--- lib/strings/split_string.c
+++ lib/strings/split_string.c
@@ -7,6 +7,8 @@
 
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdbool.h>
+
 #include "my_strings.h"
 
 static int isdelim(char c, char const *delim)
@@ -37,6 +39,17 @@ static int count_words(char const *str, char const *delim)
     return wcount;
 }
 
+void
+skip(char const *str, int *i, char const *delim, bool skip_delim)
+{
+    if (skip_delim)
+        while (isdelim(str[*i], delim))
+            (*i)++;
+    else
+        while (!isdelim(str[*i], delim))
+            (*i)++;
+}
+
 char **split_string(char const *str, char const *delim)
 {
     int wcount = count_words(str, delim);
@@ -44,12 +57,12 @@ char **split_string(char const *str, char const *delim)
     int i = 0;
     int start = 0;
 
+    if (tab == NULL)
+        return NULL;
     for (int j = 0; j < wcount; j++){
-        while (isdelim(str[i], delim))
-            i++;
+        skip(str, &i, delim, true);
         start = i;
-        while (!isdelim(str[i], delim))
-            i++;
+        skip(str, &i, delim, false);
         tab[j] = malloc(i - start + 1);
         if (tab[j]) {
             my_strncpy(tab[j], str + start, i - start);

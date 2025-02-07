fe4dce59f711f6fbbb2b074300e51a22643b2555
Théophile "Inkurey" R. <inkurey22.tr@proton.me>
2025-02-07 15:54:35 +0100

    FEAT: now supports UID & GID in sudoers

--- src/permissions.c
+++ src/permissions.c
@@ -24,15 +24,54 @@ int is_user_or_group(char *str, char *user, char **groups)
     return 0;
 }
 
+static char *get_user_or_group(int id)
+{
+    FILE *file = fopen("/etc/passwd", "r");
+    char *line = NULL;
+    size_t len = 0;
+    char **tab = NULL;
+    char *res = NULL;
+
+    if (!file)
+        return NULL;
+    while (getline(&line, &len, file) != -1) {
+        tab = split_string(line, ":");
+        if (atoi(tab[2]) == id) {
+            res = strdup(tab[0]);
+            free(tab);
+            break;
+        }
+        free(tab);
+    }
+    fclose(file);
+    free(line);
+    return res;
+}
+
+int is_id(char *str, char **groups, char *user)
+{
+    int id = 0;
+    char *tmp = NULL;
+
+    if ((str[0] < '0' || str[0] > '9') && str[0] != '%')
+        return 0;
+    id = str[0] == '%' ? atoi(str + 1) : atoi(str);
+    tmp = get_user_or_group(id);
+    if (is_user_or_group(tmp, user, groups))
+        return 1;
+    return 0;
+}
+
 static int get_perms(FILE *file, char *user, char **groups)
 {
     char *line = NULL;
     size_t len = 0;
 
     while (getline(&line, &len, file) != -1) {
-        if (line[0] == '#' || line[0] == '\n' ||
-            strncmp(line, "Defaults", 8) == 0)
+        if (line[0] == '\n' || strncmp(line, "Defaults", 8) == 0)
             continue;
+        if (line[0] == '#' && is_id(line + 1, groups, user))
+            return 0;
         if (is_user_or_group(line, user, groups))
             return 0;
     }
